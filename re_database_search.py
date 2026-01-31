d37696a768a9dd73a907de9d74196643e0921457
Alex Popp <apopp526@gmail.com>
2026-01-31 17:30:38 -0600

    Remove redundant queries

--- re_database_search.py
+++ re_database_search.py
@@ -34,46 +34,11 @@ def process_form():
 				'commercial_source': commercial_source, 'reference_number': reference_number})
 				search_results['num_matches'] += 1
 		
-		elif type == 'prototype':
-			qry = 'SELECT enzyme_name FROM final_project_data WHERE prototype LIKE (%s)'
-			curs.execute(qry, (query,))
-			for enzyme_name in curs:	
-				search_results['matches'].append({'enzyme_name': enzyme_name})
-				search_results['num_matches'] += 1
-		
 		elif type == 'microorganism':
 			qry = 'SELECT enzyme_name, microorganism FROM final_project_data WHERE microorganism LIKE (%s)'
 			curs.execute(qry, (query + '%',))
 			for (enzyme_name, microorganism) in curs:
 				search_results['matches'].append({'enzyme_name': enzyme_name, 'microorganism': microorganism})
-				search_results['num_matches'] += 1
-		
-		elif type == 'source_of_microorganism':
-			qry = 'SELECT enzyme_name FROM final_project_data WHERE source_of_microorganism LIKE (%s)'
-			curs.execute(qry, (query,))
-			for enzyme_name in curs:
-				search_results['matches'].append({'enzyme_name': enzyme_name})
-				search_results['num_matches'] += 1
-		
-		elif type == 'recognition_sequence':
-			qry = 'SELECT enzyme_name FROM final_project_data WHERE recognition_sequence LIKE (%s)'
-			curs.execute(qry, (query,))
-			for enzyme_name in curs:
-				search_results['matches'].append({'enzyme_name': enzyme_name})
-				search_results['num_matches'] += 1
-		
-		elif type == 'meth_site_type':
-			qry = 'SELECT enzyme_name FROM final_project_data WHERE meth_site_type LIKE (%s)'
-			curs.execute(qry, (query,))
-			for enzyme_name in curs:
-				search_results['matches'].append({'enzyme_name': enzyme_name})
-				search_results['num_matches'] += 1
-
-		elif type == 'commercial_source':
-			qry = 'SELECT enzyme_name FROM final_project_data WHERE commercial_source LIKE (%s)'
-			curs.execute(qry, ('%' + query + '%',))
-			for enzyme_name in curs:
-				search_results['matches'].append({'enzyme_name': enzyme_name})
 				search_results['num_matches'] += 1
 
 		elif type == 'reference_number':
@@ -81,6 +46,13 @@ def process_form():
 			curs.execute(qry, (query,))
 			for (reference_number, full_reference) in curs:
 				search_results['matches'].append({'reference_number': reference_number, 'full_reference': full_reference})
+				search_results['num_matches'] += 1
+
+		else:
+			qry = f'SELECT enzyme_name FROM final_project_data WHERE {type} LIKE (%s)'
+			curs.execute(qry, (query,))
+			for enzyme_name in curs:
+				search_results['matches'].append({'enzyme_name': enzyme_name})
 				search_results['num_matches'] += 1
 
 		curs.close()
